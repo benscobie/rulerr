79495696ea06f0f061f3fafd79df3bb7ff9106a4
Ben Scobie <benscobie@users.noreply.github.com>
2025-06-02 14:15:55 +0100

    Add turborepo, nestjs API, strict TS & linting

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# rulerr
+# Rulerr
+
 Automate your home media management with user-defined rules and seamless API integration. One system to rule them all!
+
+This project was inspired by the [Maintainerr](https://github.com/jorenn92/Maintainerr) project.
+
+## Running locally
+
+```bash
+corepack install
+corepack enable
+pnpm install
+pnpm dev
+```
---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,28 @@
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["eslint.config.mjs"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: "module",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+);
